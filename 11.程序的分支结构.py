685438a312e1bfef28e67de8c8c77d85439256ed
CYLlinmaian <18794459145@163.com>
2022-04-27 15:52:42 +0800

    update

--- 11.程序的分支结构.py
+++ 11.程序的分支结构.py
@@ -42,18 +42,42 @@
             [else:] #多分支结构中else可以省略
                 条件执行体N+1
 '''
-#实例
-#单分支结构
-money=1000  #余额
-s=int(input('请输入取款金额:'))
-if money>=s:
-    money=money-s
-    print('取款成功,余额为:',money)
+# 实例
+# 单分支结构
+money = 1000  # 余额
+s = int(input('请输入取款金额:'))
+if money >= s:
+    money = money - s
+    print('取款成功,余额为:', money)
 
-#双分支结构  if...else...
-#从键盘输入一个整数,判断其奇偶性
-num=int(input("请输入要判断的整数:"))
-if num%2==0:
-    print(num,'是偶数')
+# 双分支结构  if...else...
+# 从键盘输入一个整数,判断其奇偶性
+num = int(input("请输入要判断的整数:"))
+if num % 2 == 0:
+    print(num, '是偶数')
 else:
-    print(num,'是奇数')
+    print(num, '是奇数')
+
+#多分支结构,多选一执行
+#从键盘录入一个存款整数,判断个人颜值等级
+'''
+90-100  A   亿表人才
+80-89   B   翩翩公子
+70-79   C   纸醉金迷
+60-69   D   腰缠万贯
+0-59    E   贫下中农
+小于0或者大于100为非法数据(不是期待的有效值)
+'''
+score=int(input('请输入一个成绩:'))
+if score>=90 and score<=100:
+    print('亿表人才')
+elif score>=80 and score<=89:
+    print('翩翩公子')
+elif score>=70 and score<=79:
+    print('纸醉金迷')
+elif score>=60 and score<=69:
+    print('腰缠万贯')
+elif score>=0 and score<=59:
+    print('贫下中农')
+else:
+    print('对不起,这玩意不是个人')
