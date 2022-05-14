a1c12f4dc8d66fb8e5f6412256a5520f6a0648b6
CYLlinmaian <18794459145@163.com>
2022-05-14 09:43:56 +0800

    update

---
+++ 例程/圆的面积.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/5/14 9:30
+# @Author  : CYLlinmaian
+# @FileName: 圆的面积.py
+# @Software: PyCharm
+radius = float(input("Radius:"))
+print("The Area Is","{0:.2f}".format(3.1415926 * radius ** 2))
