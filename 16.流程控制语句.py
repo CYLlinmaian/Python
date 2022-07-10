47cc509e3504d11834fc263ef1dcc89b62a0518c
CYLlinmaian <CYLlinmaian@163.com>
2022-07-10 20:56:20 +0800

    update

--- 16.流程控制语句.py
+++ 16.流程控制语句.py
@@ -1,16 +1,16 @@
 '''
 Author: CYLlinmaian
 Date: 2022-07-10 18:50:07
-LastEditTime: 2022-07-10 20:07:55
+LastEditTime: 2022-07-10 20:56:09
 LastEditors: CYLlinmaian
 Description: 
 FilePath: \Python\16.流程控制语句.py
 Copyright © 1999 - 2022 Mr.chen.All Rights Reserved.
 '''
+
 # break语句
 # 用于结束循环，通常与分支结构if一起使用
 '''从键盘录入密码，最多输三次，密码正确结束循环'''
-
 
 for item in range(3):
     pwd = input('请输入密码：')
