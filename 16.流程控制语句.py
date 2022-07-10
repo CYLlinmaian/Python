90e35d6e465d8c8d098232d6e229a82d1fbf07e0
CYLlinmaian <CYLlinmaian@163.com>
2022-07-10 20:18:10 +0800

    update

---
+++ 16.流程控制语句.py
@@ -0,0 +1,21 @@
+'''
+Author: CYLlinmaian
+Date: 2022-07-10 18:50:07
+LastEditTime: 2022-07-10 20:07:55
+LastEditors: CYLlinmaian
+Description: 
+FilePath: \Python\16.流程控制语句.py
+Copyright © 1999 - 2022 Mr.chen.All Rights Reserved.
+'''
+# break语句
+# 用于结束循环，通常与分支结构if一起使用
+'''从键盘录入密码，最多输三次，密码正确结束循环'''
+
+
+for item in range(3):
+    pwd = input('请输入密码：')
+    if pwd == '8888':
+        print('密码正确')
+        break
+    else:
+        print('密码不正确')
